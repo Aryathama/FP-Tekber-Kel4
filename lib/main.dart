5b6f44eeb76a0346bb86330aa0fe7b422124f35f
ryanadi28 <ryanadiputrapratama35@gmail.com>
2025-06-08 08:12:47 +0700

    splash screen ketinggalan

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart'; // ⬅️ Tambahkan ini
+import 'firebase_options.dart';
+import 'dart:async';
+
+// Import screen lain
 import 'screen/login_screen.dart';
 
 void main() async {
@@ -21,8 +24,43 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Health Tracker App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.teal),
-      home: LoginScreen(),
+      home: const SplashScreen(),
+    );
+  }
+}
+
+// SplashScreen dengan background putih & logo di tengah
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => LoginScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image(
+          image: AssetImage('assets/logo.png'),
+          width: 200,
+        ),
+      ),
     );
   }
 }
